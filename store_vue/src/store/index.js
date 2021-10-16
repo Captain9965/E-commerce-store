35732cd0438df5ca7c6844fd439a18215a396d92
lenny <lennyweda@gmail.com>
2021-10-16 23:01:17 +0300

    added cart functionality

--- store_vue/src/store/index.js
+++ store_vue/src/store/index.js
@@ -2,8 +2,31 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    cart:{
+      items:[],
+    },
+    isAuthenticated: false,
+    token:'',
+    isLoading:false,
+
   },
   mutations: {
+    intiializeStore(state){
+      if (localStorage.getItem('cart')){
+        state.cart = JSON.parse(localStorage.getItem('cart'))
+      } else{
+        localStorage.setItem('cart', JSON.stringify(state.cart))
+      }
+    },
+    addToCart(state, item){
+      const exists = state.cart.items.filter(i => i.product.id === item.product.id)
+      if (exists.length){
+        exists[0].quantity = parseInt(exists[0].quantity) + parseInt(item.quantity)
+      } else{
+        state.cart.items.push(item)
+      }
+      localStorage.setItem('cart', JSON.stringify(state.cart))
+    }
   },
   actions: {
   },
