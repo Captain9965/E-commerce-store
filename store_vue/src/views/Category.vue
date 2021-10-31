483477b87cf2d2931fbc126dec74698b31b20427
lenny <lennyweda@gmail.com>
2021-10-31 20:47:28 +0300

    product box done

--- store_vue/src/views/Category.vue
+++ store_vue/src/views/Category.vue
@@ -6,26 +6,18 @@
                     {{category.name}}
                 </h2>
             </div>
-              <div 
-            class="column is-3" 
-            v-for="product in category.products"
-            v-bind:key="product.id"
-            >
-                <div class="box">
-                <figure class="image mb-4">
-                    <img v-bind:src="product.get_thumbnail">
-                </figure>
-                <h3 class="is-size-4">{{product.name}}</h3>
-                <p class="is-size-6 has-text-grey">Ksh{{product.price}}</p>
-                <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
-                </div>
-            </div>
+              <ProductBox
+                v-for = "product in category.products"
+                v-bind:key = "product.id"
+                v-bind:product = "product"
+            />
         </div>
     </div>
 </template>
 <script>
 import axios from 'axios'
 import { toast } from 'bulma-toast'
+import ProductBox from '@/components/ProductBox'
 
 export default{
     name : 'Category',
@@ -39,6 +31,16 @@ export default{
     },
     mounted(){
         this.getCategory()
+    },
+    watch:{
+        $route(to, from){
+            if (to.name === 'Category'){
+                this.getCategory()
+            }
+        }
+    },
+    components: {
+        ProductBox
     },
     methods : {
         async getCategory(){
--- store_vue/src/views/Home.vue
+++ store_vue/src/views/Home.vue
@@ -14,26 +14,18 @@
       <div class="column is-12">
         <h2 class="is-size-2 has-text-centered"> Latest stock </h2>
       </div>
-      <div 
-      class="column is-3" 
-      v-for="product in latestProducts"
-      v-bind:key="product.id"
-      >
-        <div class="box">
-          <figure class="image mb-4">
-            <img v-bind:src="product.get_thumbnail">
-          </figure>
-          <h3 class="is-size-4">{{product.name}}</h3>
-          <p class="is-size-6 has-text-grey">Ksh{{product.price}}</p>
-          <router-link v-bind:to="product.get_absolute_url" class="button is-dark mt-4">View details</router-link>
-        </div>
-    </div>
+      <ProductBox
+        v-for = "product in latestProducts"
+        v-bind:key = "product.id"
+        v-bind:product = "product"
+      />
   </div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import ProductBox from '@/components/ProductBox'
 
 export default {
   name: 'Home',
@@ -43,6 +35,7 @@ export default {
     }
   },
   components: {
+    ProductBox
   },
   mounted(){
     this.getLatestProducts()
@@ -62,10 +55,4 @@ export default {
   }
 }
 </script>
-<style scoped>
-  .image{
-    margin-top: -1.25rem;
-    margin-left: -1.25rem;
-    margin-right: -1.25rem;
-  }
-</style>
+
